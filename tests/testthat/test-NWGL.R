a539804904ae96476ed2a8f296c8a82b8b759baa
Sebastian Meyer <Sebastian.Meyer@ifspm.uzh.ch>
2014-08-22 15:04:50 +0200

    really cache nodes and weights up to nGQ=60, i.e. up to gauss.quad(n=61)

--- tests/testthat/test-NWGL.R
+++ tests/testthat/test-NWGL.R
@@ -1,8 +1,10 @@
 
 context("Validation of cached Gauss-Legendre nodes/weights")
 
-test_that("statmod::gauss.quad() still gives the same result", {
-    new.NWGL <- lapply(seq_len(60L), function (n)
-                       unname(statmod::gauss.quad(n = n, kind = "legendre")))
-    expect_that(new.NWGL, equals(.NWGL, check.attributes = FALSE))
-})
+if (requireNamespace("statmod")) {
+    test_that("statmod::gauss.quad() still gives the same result", {
+        new.NWGL <- lapply(seq_len(61L), function (n)
+                           unname(statmod::gauss.quad(n = n, kind = "legendre")))
+        expect_that(new.NWGL, equals(.NWGL, check.attributes = FALSE))
+    })
+}
