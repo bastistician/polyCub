bc9d117394ddf0e6ed937d668a61dda548c182f8
Sebastian Meyer <Sebastian.Meyer@ifspm.uzh.ch>
2014-09-27 13:49:49 +0200

    re-order examples

--- inst/examples/polyCub.R
+++ inst/examples/polyCub.R
@@ -1,4 +1,4 @@
-### Short comparison of the different cubature methods
+### Short comparison of the various cubature methods
 
 ## 2D-function to integrate (here: isotropic zero-mean Gaussian density)
 f <- function (s, sigma = 5) exp(-rowSums(s^2)/2/sigma^2) / (2*pi*sigma^2)
@@ -8,15 +8,6 @@ disc.owin <- spatstat::disc(radius=5, centre=c(3,2), npoly=8)
 
 ## plot image of the function and integration domain
 plotpolyf(disc.owin, f, xlim=c(-8,8), ylim=c(-8,8))
-
-
-### Quasi-exact cubature of the bivariate Gaussian density
-### using gpclib::tristrip and mvtnorm::pmvnorm()
-
-if (requireNamespace("mvtnorm") && gpclibPermit()) {
-    print(polyCub.exact.Gauss(disc.owin, mean=c(0,0), Sigma=5^2*diag(2),
-                              plot=TRUE), digits=16)
-}
 
 
 ### Two-dimensional midpoint rule
@@ -49,5 +40,16 @@ polyCub.SV(disc.owin, f, nGQ=2, rotation=FALSE, plot=TRUE)
 polyCub.SV(disc.owin, f, nGQ=2, rotation=TRUE, plot=TRUE)
 par(opar)
 
+
 ### Line integration along the boundary for isotropic functions
+
 polyCub.iso(disc.owin, f, center=c(0,0))   # see ?polyCub.iso
+
+
+### Quasi-exact cubature of the bivariate Gaussian density
+### using gpclib::tristrip and mvtnorm::pmvnorm()
+
+if (requireNamespace("mvtnorm") && gpclibPermit()) {
+    print(polyCub.exact.Gauss(disc.owin, mean=c(0,0), Sigma=5^2*diag(2),
+                              plot=TRUE), digits=16)
+}
