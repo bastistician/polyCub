7b24b5d91b5efb47ad7122a71a881686d382d348
Sebastian Meyer <seb.meyer@fau.de>
2017-05-23 22:31:58 +0200

    test C-implementation of polyCub1.iso()

---
+++ tests/testthat/polyiso_powerlaw.c
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Example of using the C-routine "polyCub_iso", see also test-polyiso.R
+ *
+ * Copyright (C) 2015,2017 Sebastian Meyer
+ *
+ * This file is part of the R package "polyCub",
+ * free software under the terms of the GNU General Public License, version 2,
+ * a copy of which is available at http://www.R-project.org/Licenses/.
+ ******************************************************************************/
+
+#include <math.h>
+#include "polyCubAPI.h"
+
+// F(R) example
+static double intrfr_powerlaw(double R, double *logpars)
+{
+        double sigma = exp(logpars[0]);
+        double d = exp(logpars[1]);
+        if (d == 1.0) {
+                return R - sigma * log(R/sigma + 1);
+        } else if (d == 2.0) {
+                return log(R/sigma + 1) - R/(R+sigma);
+        } else {
+                return (R*pow(R+sigma,1-d) - (pow(R+sigma,2-d) - pow(sigma,2-d))/(2-d)) / (1-d);
+        }
+}
+
+// function to be called from R
+void C_polyiso_powerlaw(
+        double *x, double *y,               // vertex coordinates (open)
+        int *L,                             // number of vertices
+        //intrfr_fn intrfr,                 // F(R)
+        double *pars,                       // parameters for F(R)
+        double *center_x, double *center_y, // center of isotropy
+        int *subdivisions, double *epsabs, double *epsrel, // Rdqags options
+        int *stop_on_error,                 // !=0 means to stop at first ier > 0
+        double *value, double *abserr, int *neval) // results
+{
+        polyCub_iso(x, y, L,
+                    intrfr_powerlaw,
+                    pars, center_x, center_y,
+                    subdivisions, epsabs, epsrel,
+                    stop_on_error,
+                    value, abserr, neval);
+        return;
+}
---
+++ tests/testthat/test-polyiso.R
@@ -0,0 +1,76 @@
+## CAVE (as of R-3.4.0 with testthat 1.0.2):
+## During R CMD check, tools:::.runPackageTests() sets R_TESTS=startup.Rs,
+## a file which is created in the parent directory "tests", see
+## file.path(R.home("share"), "R", "tests-startup.R")
+## for its contents. However, testthat tests are run with the working directory
+## set to here, so auxiliary R sessions initiated here would fail when trying
+## to source() the R_TESTS file on startup, see the system Rprofile file
+## file.path(R.home("library"), "base", "R", "Rprofile")
+## for what happens. Solution: unset R_TESTS (or set to "") for sub-R processes.
+
+context("C-implementation of polyCub1.iso()")
+
+message("compiling polyiso_powerlaw.c using R CMD SHLIB")
+shlib_error <- tools::Rcmd(args = c("SHLIB", "--clean", "polyiso_powerlaw.c"),
+                           env = c(paste0("PKG_CPPFLAGS=-I", system.file("include", package="polyCub")),
+                                   "R_TESTS=''"))
+if (shlib_error)
+    skip("failed to build the shared object/DLL for the polyCub_iso example")
+
+## load shared object/DLL
+myDLL <- paste0("polyiso_powerlaw", .Platform$dynlib.ext)
+loadNamespace("polyCub")
+dyn.load(myDLL)
+
+## R function calling C_polyiso_powerlaw
+polyiso_powerlaw <- function (xypoly, logpars, center,
+                              subdivisions = 100L, rel.tol = .Machine$double.eps^0.25,
+                              abs.tol = rel.tol, stop.on.error = TRUE)
+{
+    .C("C_polyiso_powerlaw",
+       as.double(xypoly$x), as.double(xypoly$y), as.integer(length(xypoly$x)),
+       as.double(logpars),
+       as.double(center[1L]), as.double(center[2L]),
+       as.integer(subdivisions), as.double(abs.tol), as.double(rel.tol),
+       as.integer(stop.on.error),
+       value = double(1L), abserr = double(1L), neval = integer(1L))[c("value", "abserr", "neval")]
+}
+
+
+## example polygon and function parameters
+set.seed(1)
+xy <- list(x = stats::rnorm(10), y = stats::rnorm(10))
+hidx <- grDevices::chull(xy)
+xypoly <- lapply(xy, "[", rev(hidx))  # anticlockwise vertex order
+logpars <- log(c(0.5, 1))
+
+(res <- polyiso_powerlaw(xypoly, logpars, center = c(0,0)))
+
+
+## compare with R implementation
+intrfr.powerlaw <- function (R, logpars)
+{
+    sigma <- exp(logpars[[1L]])
+    d <- exp(logpars[[2L]])
+    if (d == 1) {
+        R - sigma * log(R/sigma + 1)
+    } else if (d == 2) {
+        log(R/sigma + 1) - R/(R+sigma)
+    } else {
+        (R*(R+sigma)^(1-d) - ((R+sigma)^(2-d) - sigma^(2-d))/(2-d)) / (1-d)
+    }
+}
+(orig <- polyCub:::polyCub1.iso(xypoly, intrfr.powerlaw, logpars, center = c(0,0)))
+
+
+test_that("C and R implementations give equal results", {
+    expect_equal(res$value, orig[1])
+    expect_equal(res$abserr, orig[2])
+})
+
+## microbenchmark::microbenchmark(
+##     polyCub:::polyCub1.iso(xypoly, intrfr.powerlaw, logpars, center = c(0,0)), # 250 mus
+##     polyiso_powerlaw(xypoly, logpars, center = c(0,0)), times = 1000)          #  50 mus
+
+dyn.unload(myDLL)
+file.remove(myDLL)
