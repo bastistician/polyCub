ecf6d2f1e7e1729f190ed070ef410550696f70e4
Sebastian Meyer <seb.meyer@fau.de>
2017-05-23 14:34:37 +0200

    reduce indent (from 8 to 4 spaces)

--- inst/include/polyCubAPI.h
+++ inst/include/polyCubAPI.h
@@ -15,20 +15,20 @@
 typedef double (*intrfr_fn) (double, double*);
 
 void polyCub_iso(
-        double *x, double *y,               // vertex coordinates (open)
-        int *L,                             // number of vertices
-        intrfr_fn intrfr,                   // F(R)
-        double *pars,                       // parameters for F(R)
-        double *center_x, double *center_y, // center of isotropy
-        int *subdivisions, double *epsabs, double *epsrel, // Rdqags options
-        int *stop_on_error,                 // !=0 means to stop at first ier > 0
-        double *value, double *abserr, int *neval) // results
+    double *x, double *y,               // vertex coordinates (open)
+    int *L,                             // number of vertices
+    intrfr_fn intrfr,                   // F(R)
+    double *pars,                       // parameters for F(R)
+    double *center_x, double *center_y, // center of isotropy
+    int *subdivisions, double *epsabs, double *epsrel, // Rdqags options
+    int *stop_on_error,                 // !=0 means to stop at first ier > 0
+    double *value, double *abserr, int *neval) // results
 {
-        static void(*fun)() = NULL;
-        if (fun == NULL)
-                fun = (void (*)) R_GetCCallable("polyCub", "polyiso");
-        fun(x, y, L, intrfr, pars, center_x, center_y,
-            subdivisions, epsabs, epsrel, stop_on_error,
-            value, abserr, neval);
-        return;
+    static void(*fun)() = NULL;
+    if (fun == NULL)
+        fun = (void (*)) R_GetCCallable("polyCub", "polyiso");
+    fun(x, y, L, intrfr, pars, center_x, center_y,
+        subdivisions, epsabs, epsrel, stop_on_error,
+        value, abserr, neval);
+    return;
 }
--- src/polyCub.iso.h
+++ src/polyCub.iso.h
@@ -11,11 +11,11 @@
 typedef double (*intrfr_fn) (double, double*);
 
 void polyiso(
-        double *x, double *y,               // vertex coordinates (open)
-        int *L,                             // number of vertices
-        intrfr_fn intrfr,                   // F(R)
-        double *pars,                       // parameters for F(R)
-        double *center_x, double *center_y, // center of isotropy
-        int *subdivisions, double *epsabs, double *epsrel, // Rdqags options
-        int *stop_on_error,                 // !=0 means to stop at first ier > 0
-        double *value, double *abserr, int *neval); // results
+    double *x, double *y,               // vertex coordinates (open)
+    int *L,                             // number of vertices
+    intrfr_fn intrfr,                   // F(R)
+    double *pars,                       // parameters for F(R)
+    double *center_x, double *center_y, // center of isotropy
+    int *subdivisions, double *epsabs, double *epsrel, // Rdqags options
+    int *stop_on_error,                 // !=0 means to stop at first ier > 0
+    double *value, double *abserr, int *neval); // results
