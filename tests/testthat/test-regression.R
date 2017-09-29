5fef28e7b10ebe7e9b5039578a9a59448f5c3bbd
Sebastian Meyer <seb.meyer@fau.de>
2017-09-29 15:30:06 +0200

    replace letterR (moved to spatstat.data) by simple octagon

--- tests/testthat/test-regression.R
+++ tests/testthat/test-regression.R
@@ -1,12 +1,14 @@
 context("Regression tests")
 
+octagon <- spatstat::disc(radius = 5, centre = c(3,2), npoly = 8)
+f <- function (s) (rowSums(s^2)+1)^-2
+##plotpolyf(octagon, f)
+
 test_that("isotropic cubature can handle control list for integrate()", {
-    data("letterR", package="spatstat", envir=environment())
-    f <- function (s) (rowSums(s^2)+1)^-2
     ## previosly, passing control arguments did not work
-    int1 <- polyCub.iso(letterR, f, center=c(0,0), control=list(rel.tol=1e-3))
-    int2 <- polyCub.iso(letterR, f, center=c(0,0), control=list(rel.tol=1e-8))
-    ## results are almost identical
+    int1 <- polyCub.iso(octagon, f, center=c(0,0), control=list(rel.tol=1e-3))
+    int2 <- polyCub.iso(octagon, f, center=c(0,0), control=list(rel.tol=1e-8))
+    ## results are almost but not identical
     expect_equal(int1, int2, tolerance=1e-3)
     expect_false(identical(int1, int2))
 })
