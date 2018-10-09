d8cca9fe63087a64c4b0fa15367f86eb55118f57
Sebastian Meyer <seb.meyer@fau.de>
2018-10-09 11:22:53 +0200

    add test for nGQ > 61

--- tests/testthat/test-NWGL.R
+++ tests/testthat/test-NWGL.R
@@ -1,12 +1,16 @@
 context("Validation of cached Gauss-Legendre nodes/weights")
 
-if (requireNamespace("statmod")) {
-    test_that("statmod::gauss.quad() still gives the same result", {
-        new.NWGL <- lapply(
-            X = seq_len(61L),
-            FUN = function (n)
-            unname(statmod::gauss.quad(n = n, kind = "legendre"))
-        )
-        expect_equal(new.NWGL, .NWGL, check.attributes = FALSE)
-    })
-}
+test_that("statmod::gauss.quad() still gives the same result", {
+    new.NWGL <- lapply(
+        X = seq_len(61L),
+        FUN = function (n)
+        unname(statmod::gauss.quad(n = n, kind = "legendre"))
+    )
+    expect_equal(new.NWGL, .NWGL, check.attributes = FALSE)
+})
+
+test_that("polyCub.SV() can fetch nodes and weights from 'statmod'", {
+    diamond <- list(list(x = c(1,2,1,0), y = c(1,2,3,2)))
+    nw <- polyCub.SV(diamond, f = NULL, nGQ = 83)
+    expect_type(nw, "list")
+})
