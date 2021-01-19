9142f49f2ac40973304db171dfc64df21b2ce855
Sebastian Meyer <seb.meyer@fau.de>
2021-01-19 20:17:34 +0100

    simplify letterR

--- examples/plotpolyf.R
+++ examples/plotpolyf.R
@@ -1,11 +1,9 @@
-### a polygonal domain (a rounded version of spatstat.data::letterR$bdry)
+### a polygonal domain (a simplified version of spatstat.data::letterR$bdry)
 letterR <- list(
-    list(x = c(3.9, 3.8, 3.7, 3.5, 3.4, 3.5, 3.7, 3.8, 3.8, 3.7,
-               3.7, 3.5, 3.3, 2, 2, 2.7, 2.7, 2.9, 3, 3.3, 3.9),
-         y = c(0.7, 1.1, 1.3, 1.7, 1.8, 1.9, 2.1, 2.3, 2.5, 2.8, 3,
-               3.2, 3.3, 3.3, 0.7, 0.7, 1.7, 1.7, 1.5, 0.7, 0.6)),
-    list(x = c(2.6, 2.6, 3, 3.1, 3.2, 3.1, 3.1, 3),
-         y = c(2.2, 2.7, 2.7, 2.6, 2.5, 2.4, 2.3, 2.2))
+    list(x = c(2.7, 3, 3.3, 3.9, 3.7, 3.4, 3.8, 3.7, 3.4, 2, 2, 2.7),
+         y = c(1.7, 1.6, 0.7, 0.7, 1.3, 1.8, 2.2, 2.9, 3.3, 3.3, 0.7, 0.7)),
+    list(x = c(2.6, 2.6, 3, 3.2, 3),
+         y = c(2.2, 2.7, 2.7, 2.5, 2.2))
 )
 
 ### f: isotropic exponential decay
